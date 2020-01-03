cc434fc57cacf2fb521165fc7d8a35cd636d5684
Andrew Neuhaus <aneuhaus@ucvts.tec.nj.us>
2020-01-03 09:24:22 -0500

    this is all i've got, sorry

---
+++ HiddenWord.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class HiddenWord {
+     private static Scanner in = new Scanner(System.in);
+     
+     
+     public static void main(String[] args) {
+          
+     }
+}
